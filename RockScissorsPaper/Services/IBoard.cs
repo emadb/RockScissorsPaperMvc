a639a1db9f851adf652c3a9a4854ebb929c3ce1e
ema <emanuele@codiceplastico.com>
2011-10-07 17:30:33 +0200

    introduced Board and Randomizer

--- RockScissorsPaper/Services/IBoard.cs
+++ RockScissorsPaper/Services/IBoard.cs
@@ -1,8 +1,52 @@
-﻿namespace RockScissorsPaper.Services
+﻿using System;
+
+namespace RockScissorsPaper.Services
 {
     public interface IBoard
     {
         Winner PlayRock();
+    }
+
+    public class Board : IBoard
+    {
+        private readonly IRandomizer _randomizer;
+
+        public Board(IRandomizer randomizer)
+        {
+            _randomizer = randomizer;
+        }
+
+        public Winner PlayRock()
+        {
+            Symbols computer = _randomizer.Next();
+            if (computer == Symbols.Paper)
+                return Winner.Computer;
+            if (computer == Symbols.Scissors)
+                return Winner.User;
+            return Winner.None;
+        }
+    }
+
+    public interface IRandomizer
+    {
+        Symbols Next();
+    }
+
+    class Randomizer : IRandomizer
+    {
+        public Symbols Next()
+        {
+            Random random = new Random(DateTime.Now.Millisecond);
+            return (Symbols)random.Next(1, 3);
+        }
+    }
+
+    public enum Symbols
+    {
+        None,
+        Rock,
+        Scissors,
+        Paper
     }
 
     public enum Winner
